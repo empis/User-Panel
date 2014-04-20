7073aa6c677042f90bbce288fc716ad903ff82b1
empis <admin@empis.eu>
2014-04-20 22:54:11 +0200

    Init

---
+++ login.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>USER PANEL | CooperPack Server</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="ddaccordion.js"></script>
+<script type="text/javascript">
+ddaccordion.init({
+	headerclass: "submenuheader", //Shared CSS class name of headers group
+	contentclass: "submenu", //Shared CSS class name of contents group
+	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
+	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
+	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc] [] denotes no content
+	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+	animatedefault: false, //Should contents open by default be animated into view?
+	persiststate: true, //persist state of opened contents within browser session?
+	toggleclass: ["", ""], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+	togglehtml: ["suffix", "<img src='images/plus.gif' class='statusicon' />", "<img src='images/minus.gif' class='statusicon' />"], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
+		//do nothing
+	}
+})
+</script>
+
+<script type="text/javascript" src="jconfirmaction.jquery.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function() {
+		$('.ask').jConfirmAction();
+	});
+	
+</script>
+
+<script language="javascript" type="text/javascript" src="niceforms.js"></script>
+<link rel="stylesheet" type="text/css" media="all" href="niceforms-default.css" />
+
+</head>
+<?
+//Function
+function check_password_db($nickname,$password) {
+    $link = mysql_connect('46.36.35.188', 'empis', 'cooperpackheslo') or die('Could not connect: ' . mysql_error());
+	mysql_select_db('refsys') or die('Could not select database');
+	$a=mysql_query("SELECT password FROM authme where username = '$nickname'");
+	if(mysql_num_rows($a) == 1 ) {
+	   $password_info=mysql_fetch_array($a);
+	   $sha_info = explode("$",$password_info[0]);
+	 } else return false;
+	if( $sha_info[1] === "SHA" ) {
+						$salt = $sha_info[2];
+						$sha256_password = hash('sha256', $password);
+						$sha256_password .= $sha_info[2];;
+						if( strcasecmp(trim($sha_info[3]),hash('sha256', $sha256_password) ) == 0 ) return true;
+						else return false;
+	}
+
+}
+//---------------------
+session_start();
+//Odhlasenie
+if($_GET['page'] == 'logout')
+{
+$_SESSION["logined"]=0;
+echo '
+<center><img src="images/valid.png"><font color="green">Úspešne odhlásený.</font></center>
+';
+}
+//-----------------------
+if (isset($_POST['prihlasit'])) {
+$meno = $_POST['menoo'];
+$heslo = $_POST['hesloo'];
+if($meno == "" || $heslo == ""){
+echo '
+<center><img src="images/error.png"><font color="red">Nezadal si meno alebo heslo.</font></center>
+';
+}else{
+$spravnost = check_password_db($meno, $heslo);
+if($spravnost == false){
+echo '
+<center><img src="images/error.png"><font color="red">Zadal si nesprávne údaje.</font></center>
+';
+}else{
+$_SESSION['logined']=1;
+$_SESSION['loginmeno']=$meno;
+$link = mysql_connect('46.36.35.188', 'empis', 'cooperpackheslo') or die('Could not connect: ' . mysql_error());
+mysql_select_db('refsys') or die('Could not select database');
+$b=mysql_query("SELECT admin FROM authme where username = '$meno'");
+$c=mysql_fetch_array($b);
+if($c[0] == 1){
+	$_SESSION['admin']=1;
+}
+header("Location: index.php");
+}
+}
+}
+?>
+<body>
+<div id="main_container">
+
+	<div class="header_login">
+    
+    </div>
+
+     
+         <div class="login_form">
+         
+         <h3>User Panel Login</h3>
+         
+         <form action="?" method="post" class="niceform">
+         
+                <fieldset>
+                    <dl>
+                        <dt><label for="email">Meno:</label></dt>
+                        <dd><input type="text" name="menoo" id="" size="54" /></dd>
+                    </dl>
+                    <dl>
+                        <dt><label for="password">Heslo:</label></dt>
+                        <dd><input type="password" name="hesloo" id="" size="54" /></dd>
+                    </dl>
+                    
+                    <dl>
+                        <dt><label></label></dt>
+                        <dd>
+                    <input type="checkbox" name="interests[]" id="" value="" /><label class="check_label">Zapametať si ma</label>
+                        </dd>
+                    </dl>
+                    
+                     <dl class="submit">
+                    <input type="submit" name="prihlasit" id="submit" value="Prihlásiť sa" />
+                     </dl>
+                    
+                </fieldset>
+                
+         </form>
+         </div>  
+          
+	
+    
+    <div class="footer_login">
+    
+    	<div class="left_footer_login">USER PANEL | Coded by EMPIS, Design by <a href="http://indeziner.com">INDEZINER</a></div>
+    	<div class="right_footer_login"><a href="http://indeziner.com"><img src="images/indeziner_logo.gif" alt="" title="" border="0" /></a></div>
+    
+    </div>
+
+</div>		
+</body>
+</html>
---
+++ redeemcode.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>USER PANEL | CooperPack Server</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="ddaccordion.js"></script>
+<script type="text/javascript">
+ddaccordion.init({
+	headerclass: "submenuheader", //Shared CSS class name of headers group
+	contentclass: "submenu", //Shared CSS class name of contents group
+	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
+	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
+	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc] [] denotes no content
+	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+	animatedefault: false, //Should contents open by default be animated into view?
+	persiststate: true, //persist state of opened contents within browser session?
+	toggleclass: ["", ""], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+	togglehtml: ["suffix", "<img src='images/plus.gif' class='statusicon' />", "<img src='images/minus.gif' class='statusicon' />"], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
+		//do nothing
+	}
+})
+</script>
+<script src="jquery.jclock-1.2.0.js.txt" type="text/javascript"></script>
+<script type="text/javascript" src="jconfirmaction.jquery.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function() {
+		$('.ask').jConfirmAction();
+	});
+	
+</script>
+<script type="text/javascript">
+$(function($) {
+    $('.jclock').jclock();
+});
+</script>
+
+<script language="javascript" type="text/javascript" src="niceforms.js"></script>
+<link rel="stylesheet" type="text/css" media="all" href="niceforms-default.css" />
+<?
+session_start();
+if($_SESSION["logined"] != "1"){
+header("Location: login.php");
+}
+if(isset($_POST["logoutfs"])){
+$_SESSION["logined"] = "0";
+header("Location: index.php");
+}
+//----------------------
+?>
+</head>
+<body>
+<div id="main_container">
+
+	<div class="header">
+    <div class="logo"><a href="#"><img src="images/logo.gif" alt="" title="" border="0" /></a></div>
+    
+    <div class="right_header">Vitaj <? echo $_SESSION["loginmeno"]; if($_SESSION["admin"] == 1){ echo " | Admin Účet"; } ?>, <a href="http://cooperpack.empis.eu/">Navštíviť stránku</a> | <a href="login.php?page=logout" class="logout">Odhlásiť sa</a></div>
+    <div class="jclock"></div>
+    </div>
+    
+    <div class="main_content">
+    
+                    <div class="menu">
+                    <ul>
+                    <li><a href="index.php">User Info</a></li>
+                    <li><a class="current" href="redeemcode.php">Použitie kódu<!--[if IE 7]><!--></a><!--<![endif]-->
+                    <!--[if lte IE 6]><table><tr><td><![endif]-->
+                        
+                    <!--[if lte IE 6]></td></tr></table></a><![endif]-->
+                    </li>
+                    <li><a>Nastavenia<!--[if IE 7]><!--></a><!--<![endif]-->
+                    <!--[if lte IE 6]><table><tr><td><![endif]-->
+                        <ul>
+                        <li><a href="passchange.php" title="">Zmena hesla</a></li>
+                        <!--[if lte IE 6]><table><tr><td><![endif]-->
+                        <!--[if lte IE 6]></td></tr></table></a><![endif]-->
+                        </ul>
+                    <!--[if lte IE 6]></td></tr></table></a><![endif]-->
+                    </li>
+                    <li><a href="contact.php">Kontakt</a></li>
+					<?
+						if($_SESSION["admin"] == 1){
+							echo '<li><a>Admin</a>
+								 <!--[if lte IE 6]><table><tr><td><![endif]-->
+								 <ul>
+								 <li><a href="command.php" title="">Príkaz</a></li>
+								 <li><a href="codecreate.php" title="">Vytvorenie kódov</a></li>
+								 <li><a href="changeacc.php" title="">Nastavenie účtov</a></li>
+								 </ul>
+								 <!--[if lte IE 6]></td></tr></table></a><![endif]-->
+								 </li>';
+						}
+					?>
+                    </ul>
+                    </div> 
+                    
+                    
+                    
+                    
+    <div class="center_content">  
+    
+    
+    
+    <div class="left_content">
+    
+    		</div>
+    
+    <div class="right_content">            
+        
+    <h2>Použiť kód</h2> 
+						<?
+					if (isset($_POST['pouzitkod'])) {
+$kod = $_POST['zadanykod'];
+$hrac = $_SESSION['loginmeno'];
+if($kod == ""){
+echo '<center><img src="images/error.png"><font color="red">Nezadal si kód.</font></center>';
+}else{
+//connect to db
+$link = mysql_connect('46.36.35.188', 'kody', 'cooperpackheslo')
+    or die('Could not connect: ' . mysql_error());
+mysql_select_db('kody') or die('Could not select database');
+$query = mysql_query("SELECT `code`, `player`, `usage`, `time` FROM `code` WHERE `code` = '$kod'");
+$row = mysql_fetch_array($query, MYSQL_ASSOC);
+$playerrow = $row["player"];
+$timerow = $row["time"];
+if($row["code"] != $kod){ echo '<center><img src="images/error.png"><font color="red">Zadany kod neexistuje, over ci si ho zadal spravne, ak problem pretrva kontaktujte admina.</font></center>'; }else{
+if($row["player"] != ""){ echo "<center><img src='images/error.png'><font color='red'>Kod uz bol pouzity hracom $playerrow</font></center>"; }else{
+if($row["usage"] == "cooperpackvip"){
+    $time = $row["time"] * 86400;
+    include_once 'Websend.php';
+    
+    $ws = new Websend("cp.empis.eu");
+    $ws->password = "supertajneheslocooperpack";
+    
+    if($ws->connect()){
+        $ws->doCommandAsConsole('pex user '.$hrac.' group add vip "" '.$time);
+        $ws->disconnect();
+	    mysql_query("UPDATE `code` SET `player`='$hrac' WHERE `code`='$kod'");
+		echo "<center><img src='images/valid.png'><font color='green'>Vip aktivovane na $timerow dni pre $hrac na servery CooperPack.</font></center>";
+    }else{
+        echo '<center><img src="images/error.png"><font color="red">Nepodarilo sa pripojiť.</font></center>';
+    }
+}//koniec cooperpackvip
+} //koniec overenia pouzitia
+} //koniec overenia kodu
+}}
+					?>
+<div class="form">
+         <form action="" method="post" class="niceform">
+         
+                <fieldset>
+                    <dl>
+                        <dt><label for="email">Kód:</label></dt>
+                        <dd><input type="text" name="zadanykod" id="" size="54" /></dd>
+                    </dl>
+					<dl class="submit">
+                    <input type="submit" name="pouzitkod" id="submit" value="Použiť kód" />
+                     </dl>
+                </fieldset>
+				</div>
+     </div><!-- end of right content-->
+            
+                    
+  </div>   <!--end of center content -->               
+                    
+                    
+    
+    
+    <div class="clear"></div>
+    </div> <!--end of main content-->
+	
+    
+    <div class="footer">
+    
+    	<div class="left_footer">USER PANEL | Coded by EMPIS, Design by <a href="http://indeziner.com">INDEZINER</a></div>
+    	<div class="right_footer"><a href="http://indeziner.com"><img src="images/indeziner_logo.gif" alt="" title="" border="0" /></a></div>
+    
+    </div>
+
+</div>		
+</body>
+</html>
